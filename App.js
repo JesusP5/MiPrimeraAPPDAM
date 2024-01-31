b4d1e4e95a986557c81809ca4cdebe8a869b9d12
JesusP5 <jesuson567@mechatooling.com.mx>
2024-01-31 14:35:24 -0600

    Minor changes

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App=()=> {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textHelloWorld}>Hola mundo</Text>
+      <Text style={styles.textSecondaryText} >Me llamo Jesus</Text>
+      <Text style={styles.textThirdText}>Estamos aqui en la casita</Text>
     </View>
   );
 }
@@ -13,8 +14,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textHelloWorld:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    fontFamily: 'arial'
+  },
+  textSecondaryText:{
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    color: 'white',
+    fontFamily: 'arial'
+  
+  },
+  textThirdText:{
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    color: 'white',
+    fontFamily: 'arial'
+  
+  }
 });
+export default App;
